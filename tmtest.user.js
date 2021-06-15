519555ad68e04b762e83f904c23fcddc264d10f5
Stothe <83222580+Stothe@users.noreply.github.com>
2021-06-15 11:39:36 -0400

    added update download and namespace urls

--- tmtest.user.js
+++ tmtest.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/Stothe/tampermonkeytesting
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        http://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @downloadurl  https://github.com/Stothe/tampermonkeytesting/raw/main/tmtest.user.js
+// @updateurl    https://github.com/Stothe/tampermonkeytesting/raw/main/tmtest.user.js
+
+
 // ==/UserScript==
 
 (function() {
